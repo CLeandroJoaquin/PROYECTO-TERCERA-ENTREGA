154c4602d1d3b3f75e41e58c3cfffd006897343c
CLeandroJoaquin <122206165+CLeandroJoaquin@users.noreply.github.com>
2023-07-29 19:20:29 -0300

    Add files via upload
    Cambio de archivos de CLASES

--- TERCERA_PRE_ENTREGA/carpeta_principal/Personal/views.py
+++ TERCERA_PRE_ENTREGA/carpeta_principal/Personal/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-from Personal.models import Operador, Supervisor, Gerente
+from Personal.models import Personal, Supervisor, Gerente
 # Create your views here.
 
 
-def listar_operarios(request):
+def listar_personal(request):
     contexto = {
-        "Personal": Operador.objects.all(),
+        "Personal": Personal.objects.all(),
     }
     http_response = render(
         request=request,
-        template_name='carpeta_principal/lista_operarios.html',
+        template_name='Personal/lista_personal.html',
         context=contexto,
     )
     return http_response
